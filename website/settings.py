5a0ed9b43ee6212441fb28f07823e15dfa71e29b
Grayson Gilmore <gilmoreg@live.com>
2017-06-26 18:38:30 -0500

    Fix db settings to match Heroku

--- website/settings.py
+++ website/settings.py
@@ -73,6 +73,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'website.wsgi.application'
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
--- website/settings_secret.py
+++ website/settings_secret.py
@@ -10,15 +10,3 @@ try:
     SECRET_KEY = os.environ['SECRET_KEY']
 except KeyError:
     SECRET_KEY = 'uf1og^)2nc6bgr@-qgxr@^4#)b1l3qty)_t84x8%^pt-$(v&)$'
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'pick',
-        'USER': 'pickadmin',
-        'PASSWORD': 'p1ckd3m0',
-        'HOST': DATABASE_URL,
-        'PORT': '',
-    }
-}
